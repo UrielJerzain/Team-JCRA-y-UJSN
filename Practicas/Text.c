134dfdee4c63337b2620f3c3d3de10f4e161aa24
Jose Carlos Rodriguez <jcarlos.rodriguez@edu.uag.mx>
2020-09-26 12:03:05 -0500

    Conflicto 2
    jeje

--- Practicas/Text.c
+++ Practicas/Text.c
@@ -2,7 +2,8 @@
 main
 {
     printf("My name is UJ");
+    printf("I like icecream");
     printf("My name is JC");
-    printf("My name is JC");   
+    printf("My name is JCRA");   
     printf("My name is UJ");
 }
