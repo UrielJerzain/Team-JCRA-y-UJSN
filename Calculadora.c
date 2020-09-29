60b9867f74c223e14c98ef062a44164ded92698e
Uriel Jerzain <uriel_jerza@hotmail.com>
2020-09-29 07:20:55 -0500

    Avance Calcu

---
+++ Calculadora.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+typedef unsigned char uint8;
+typedef unsigned char uint16;
+typedef unsigned char uint32;
+
+void main ( void )
+{
+	uint32 u32Variable=0
+	uint32 u32SOV=0
+	uint32 u32Opciones=0
+	uint32 u32Correr=0
+}
