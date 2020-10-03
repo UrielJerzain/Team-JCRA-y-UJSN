c90390531b6c5e20a11a58b456361847dddc289c
Uriel Jerzain <uriel_jerza@hotmail.com>
2020-10-03 14:24:24 -0500

    New files 1.1

--- Practicas/LAB01/GENFUN.c
+++ Practicas/LAB01/GENFUN.c
@@ -1,1 +1,27 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <GENFUN.h>
+
+void main (void)
+{
+	uint8 au8TestArray[11] = "hello world";
+	printf("Array before CapsOn: %s\n", au8TestArray);
+	GENFUN_vCpasOn (&au8TestArray[0], 11);
+	printf("Array after CapsOn: %s\n", au8TestArray);
+}
+
+void GENFUN_vCpasOn (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+	while ( u8SizeOfList != 0 )
+	 {
+	 	if (*pu8Src > ACII_LOW_THRESHOLD_OFF_ON && *pu8Src < ACII_HIGH_THRESHOLD_OFF_ON)
+	    {
+	     	*pu8Src -= ACII_CONVERTION_FACTOR;
+	    }
+	    else
+	    {
+	    	/*Nothing to do */
+	    }
+	    pu8Src++;
+	    u8SizeOfList--;
+	 }
+}
--- Practicas/LAB01/GENFUN.h
+++ Practicas/LAB01/GENFUN.h
@@ -1,2 +1,13 @@
 
+/*Data Types*/
 typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+/*Definitions*/
+#define ACII_HIGH_THRESHOLD_OFF_ON 123
+#define ACII_LOW_THRESHOLD_OFF_ON 96
+#define ACII_CONVERTION_FACTOR 32
+
+/*Functions Prototypes*/
+void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList);
