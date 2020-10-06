cea157b297afe73daae39aed6cb7ecf58a479008
Jose Carlos Rodriguez <jcarlos.rodriguez@edu.uag.mx>
2020-10-06 17:53:49 -0500

    Caps On Works

---
+++ Practicas/LAB01/GENFUN.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "GENFUN.h"
+
+void main (void)
+{
+
+	uint8 au8TestArray[11] = "hello world";
+	printf("Array before CapsOn: %s\n", au8TestArray);
+	GENFUN_vCapsOn (&au8TestArray[0], 11);
+	printf("Array after CapsOn: %s\n", au8TestArray);
+
+}
+
+void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+	while ( u8SizeOfList != 0 )
+	 {
+	 	if (*pu8Src > ACII_LOW_THRESHOLD_OFF_ON && *pu8Src < ACII_HIGH_THRESHOLD_OFF_ON)
+	    {
+	     	*pu8Src -= ACII_CONVERTION_FACTOR;
+	    }
+	    else
+	    {
+	    	/*Nothing to do */
+	    }
+	    pu8Src++;
+	    u8SizeOfList--;
+	 }
+}
