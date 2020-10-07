d29c78c908adbf22fe2d351f173b7b8652c5710e
Jose Carlos Rodriguez <jcarlos.rodriguez@edu.uag.mx>
2020-10-06 20:16:05 -0500

    GetAverage Works

--- Practicas/LAB01/GENFUN.c
+++ Practicas/LAB01/GENFUN.c
@@ -3,12 +3,27 @@
 
 void main (void)
 {
-
-	uint8 au8TestArray[11] = "hello world";
+	uint8 au8TestArray[12] = "hello world";
+	uint8 au8Test2Array[10] = {8,9,26,13,2,3,4,2,3,10};
+	uint8 u8Target;
+	uint8 u8Repetition;
+	uint8 u8Average;
+	printf("First function CapsOn \n");
 	printf("Array before CapsOn: %s\n", au8TestArray);
-	GENFUN_vCapsOn (&au8TestArray[0], 11);
+	GENFUN_vCapsOn (&au8TestArray[0], 12);
 	printf("Array after CapsOn: %s\n", au8TestArray);
-
+	printf("Second function CapsOff \n");
+	printf("Array before CapsOff: %s\n", au8TestArray);
+	GENFUN_vCapsOff (&au8TestArray[0], 12);
+	printf("Array after CapsOff: %s\n", au8TestArray);
+	printf("Third function GetOccurence \n");
+	printf("Type in the letter you are looking for\n");
+	scanf("%c", &u8Target);
+	u8Repetition = GENFUN_u8GetOccurence (&au8TestArray[0], u8Target,12);
+	printf("The amount of times that the letter %c was found in the array: %d\n", u8Target, u8Repetition);
+	printf("Fourth function GetAverage \n");
+	u8Average = GENFUN_u8GetAverage (&au8Test2Array[0], 10);
+	printf("The average of the array is: %d\n", u8Average);
 }
 
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
@@ -26,4 +41,56 @@ void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
 	    pu8Src++;
 	    u8SizeOfList--;
 	 }
+}
+
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+	while ( u8SizeOfList != 0 )
+	 {
+	 	if (*pu8Src > ACII_LOW_THRESHOLD_ON_OFF && *pu8Src < ACII_HIGH_THRESHOLD_ON_OFF)
+	    {
+	     	*pu8Src += ACII_CONVERTION_FACTOR;
+	    }
+	    else
+	    {
+	    	/*Nothing to do */
+	    }
+	    pu8Src++;
+	    u8SizeOfList--;
+	 }
+}
+
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList)
+{
+	uint8 u8Occurence = 0;
+	while ( u8SizeOfList != 0 )
+	{
+		if (*pu8Src == u8Target)
+		{
+			u8Occurence++;
+		}
+		else
+	    {
+	    	/*Nothing to do */
+	    }
+	    pu8Src++;
+	    u8SizeOfList--;
+	}
+	return u8Occurence;
+}
+
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+	uint8 u8Average = 0;
+	uint8 u8ListSize = 0;
+	uint8 u8Summatory = 0;
+	u8ListSize = u8SizeOfList;
+	while ( u8SizeOfList != 0 )
+	{
+		u8Summatory += *pu8Src;
+	    pu8Src++;
+	    u8SizeOfList--;
+	}
+	u8Average= u8Summatory/ u8ListSize;
+	return u8Average;
 }
--- Practicas/LAB01/GENFUN.h
+++ Practicas/LAB01/GENFUN.h
@@ -8,6 +8,11 @@ typedef unsigned int uint32;
 #define ACII_HIGH_THRESHOLD_OFF_ON 123
 #define ACII_LOW_THRESHOLD_OFF_ON 96
 #define ACII_CONVERTION_FACTOR 32
+#define ACII_LOW_THRESHOLD_ON_OFF 64
+#define ACII_HIGH_THRESHOLD_ON_OFF 91
 
 /*Functions Prototypes*/
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList);
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList);
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList);
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList);
