5001e4d3a42fc47c22de464cdb12af61c70fd010
Uriel Jerzain <uriel_jerza@hotmail.com>
2020-10-23 21:13:47 -0500

    Change .c

--- Practicas/LuhnUriel.c
+++ Practicas/LuhnUriel.c
@@ -1,16 +1,19 @@
-#include "stdio.h"
-#include "LuhnUriel.h"
+#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include "LuhnUriel.h"
 void main (void)
 {
-	uint8 au8CardNumber[16] = {4,4,6,3,5,8,5,2,3,6,3,7,8,1,6,3};
+	uint8 au8CardNumber[16] = {4,1,6,8,8,1,8,8,4,4,4,4,7,1,1,0};
 	uint8 u8Check = 0;
 	uint8 Sol;
-
+    uint8 i;
 	u8Check = u8LuhnCheck(&au8CardNumber[0],16);
-	printf("El resultado es : %d\n",u8Check);
+    for ( i = 0 ; i < 16 ; i++) 
+    {
+        printf(" %d ", au8CardNumber[i]);
+    }
+	printf("\nEl residuo es : %d\n",u8Check);
 	if ( u8Check == 0)
 	{
 	printf("Su tarjeta ha sido aceptada");
@@ -25,34 +28,38 @@ void main (void)
 uint8 u8LuhnCheck (uint8 *pu8Data, uint8 u8Sol)
 {
 	uint8 u8Check=0;
+    uint8 u8DigitoValidacion;
 	while (u8Sol != 0)
 	{
-		if( u8Sol %2 == 1 )
+		if( u8Sol %2 == 0 )
 		{
-		*pu8Data= *pu8Data * 2;
-			if (*pu8Data >= 10)
+		    *pu8Data = *pu8Data * 2;
+			if ( *pu8Data >= 10 )
 			{
-			u8Check = u8Check + *pu8Data%10 + 1;
+			    u8Check = u8Check + *pu8Data%10 + 1;
+                *pu8Data = *pu8Data%10 + 1;
 			}
 			else if (*pu8Data <=9)
 			{
-			u8Check = u8Check + *pu8Data;
+			    u8Check = u8Check + *pu8Data;
 			}	
 			else 
 			{
-			/*Nothing to do*/
+			    /*Nothing to do*/
 			}
 	
 		
 		}
 		else
 		{
-		u8Check = u8Check + *pu8Data;
+		    u8Check = u8Check + *pu8Data;
 		}
-		pu8Data++;
+		*pu8Data++;
 		u8Sol--;
-		}
-		u8Check = u8Check%10;
-		return u8Check;
+	}
+    *pu8Data--;
+    *pu8Data = u8Check%10;
+	u8DigitoValidacion = u8Check%10;
+	return (u8Check+u8DigitoValidacion)%10;
 
 }
