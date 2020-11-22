0b032fee1de5755ea4dccdfb41b701da430f41b3
Jose Carlos Rodriguez <jcarlos.rodriguez@edu.uag.mx>
2020-11-22 17:11:47 -0600

    Avances Significativos

--- Proyecto Final/McDonalds.c
+++ Proyecto Final/McDonalds.c
@@ -2,41 +2,43 @@
 #include "stdlib.h"
 #include "McDonalds.h"
 #include <time.h>
+#include <math.h>
+
+uint8 u8Ingredients[]={MAX_LETTUCE,MAX_TOMATO,MAX_MEAT,MAX_BREAD,MAX_CHEESE};
 
 void main (void)
 {
 	uint8 u8Options;
-	uint8 u8x=FACTOR;
 	printf("Welcome to McDonalds.\nWhere do you prefer to order?\n1) Inside the restaurant.\n2) Drive thru.\n");
 	scanf("%d", &u8Options);
-	while(u8x==FACTOR)
+	while(u8Options<RESTAURANT || u8Options>DRIVE_THRU)
 	{
-		if(u8Options==RESTAURANT)
-		{
-			Restaurant();
-			u8x=0;
-		}
-		else if(u8Options==DRIVE_THRU)
-		{
-			Drive_Thru();
-			u8x=0;
-		}
-		else
-		{
-			fflush(stdin);
-			scanf("%d", &u8Options);
-		}
+		fflush(stdin);
+		printf("Please select a correct option:\n1) Inside the restaurant.\n2) Drive thru.\n"); 
+		scanf("%d", &u8Options);
+	}
+	if(u8Options==RESTAURANT)
+	{
+		Restaurant();
+	}
+	else if(u8Options==DRIVE_THRU)
+	{
+		Drive_Thru();
+	}
+	else
+	{
+		/*Nothing to do*/
 	}
 }
 
 void Restaurant()
 {
-
+	Menu();
 }
 
 void Drive_Thru()
 {
-
+	Menu();
 }
 
 void delay(uint8 number_of_seconds) 
@@ -60,3 +62,246 @@ uint8 printRandoms(uint8 lower, uint8 upper,  uint8 count)
     //printf("%d \n", num); 
     return num;
 } 
+void Menu()
+{
+	uint8 u8Order;
+	uint8 u8x=FACTOR;
+	while(u8x==FACTOR)
+	{
+		printf("What would you like to order?\n1)Burgers\n2)Drinks\n3)Desserts\n4)Fries\n");
+		scanf("%d",&u8Order); 
+		while(u8Order<LIMIT1 || u8Order>LIMIT2) 
+		{ 
+			fflush(stdin); 
+			printf("Please select a correct option:\n1)Burgers\n2)Drinks\n3)Desserts\n4)Fries\n"); 
+			scanf("%d", &u8Order); 
+		} 
+    	switch(u8Order)
+    	{
+    		case OPTION1: Burgers();
+    		u8x=Extra();
+    		break;
+    		case OPTION2: /**/Drinks();
+    		u8x=Extra();
+    		break;
+    		case OPTION3: Desserts();
+    		u8x=Extra();
+    		break;
+    		case OPTION4: Fries();
+    		u8x=Extra();
+    		break;
+    	}
+	}	
+}
+
+void Burgers()
+{
+	uint8 u8Burger;
+	printf("Select a burger\n1)Big Mac\n2)Cheeseburger\n3)Quarter Pounder\n4)Deluxe\n");
+	scanf("%d",&u8Burger); 
+	while(u8Burger<LIMIT1 || u8Burger>LIMIT2) 
+	{ 
+		fflush(stdin); 
+		printf("Please select a correct option:\n1)Big Mac\n2)Cheeseburger\n3)Quarter Pounder\n4)Deluxe\n"); 
+		scanf("%d", &u8Burger); 
+	} 
+	AmountBurgers(u8Burger);
+}
+
+
+void Drinks()
+{	
+	uint8 u8Drinks;
+	printf("Select a drink:\n1)Sprite\n2)Coke\n3)Fanta\n4)Squirt\n");
+	scanf("%d",&u8Drinks);
+	while(u8Drinks<LIMIT1 || u8Drinks>LIMIT2) 
+	{ 
+		fflush(stdin); 
+		printf("Please select a correct option:\n1)Sprite\n2)Coke\n3)Fanta\n4)Squirt\n"); 
+		scanf("%d", &u8Drinks); 
+	} 
+	AmountDrinks();
+}
+
+void Desserts()
+{
+	uint8 u8Dessert;
+	printf("Select a dessert:\n1)Cheesecake\n2)Lemon Pie\n3)Ice Cream\n4)Nutella Cake\n");
+	scanf("%d",&u8Dessert);
+	while(u8Dessert<LIMIT1 || u8Dessert>LIMIT2) 
+	{ 
+		fflush(stdin); 
+		printf("Please select a correct option:\n1)Cheesecake\n2)Lemon Pie\n3)Ice Cream\n4)Nutella Cake\n"); 
+		scanf("%d", &u8Dessert); 
+	} 
+	AmountDesserts();
+}
+
+void Fries()
+{
+	uint8 u8Size;
+	printf("What Size do you prefer?\n1)Small\n2)Medium\n3)Large\n4)Extra-Large\n");
+	scanf("%d",&u8Size);
+	while(u8Size<LIMIT1 || u8Size>LIMIT2) 
+	{ 
+		fflush(stdin); 
+		printf("Please select a correct option:\n1)Small\n2)Medium\n3)Large\n4)Extra-Large\n"); 
+		scanf("%d", &u8Size); 
+	} 
+	AmountFries();	
+}
+
+uint8 Extra()
+{
+	uint8 u8Confirm; 
+	printf("Do you want to order anything else?\n1)Yes\n2)No\n"); 
+	scanf("%d",&u8Confirm); 
+	while(u8Confirm<OPTION1 || u8Confirm>OPTION2) 
+	{ 
+		fflush(stdin); 
+		printf("Please select a correct option:\n1)Yes\n2)No\n"); 
+		scanf("%d", &u8Confirm); 
+	} 
+	return u8Confirm; 
+}
+
+void AmountBurgers(uint8 u8Type)
+{
+	uint8 u8Amount;
+	uint8 u8AmountLettuce;
+	uint8 u8AmountBread;
+	uint8 u8AmountMeat;
+	uint8 u8AmountTomato;
+	uint8 u8AmountCheese;
+	printf("How many Burgers do you want?\n");
+	scanf("%d", &u8Amount);
+	while(u8Amount<0)
+	{
+		fflush(stdin);
+		printf("Type in a correct number\n");
+		scanf("%d", &u8Amount);
+	}
+	if(u8Type==OPTION1)
+	{
+		u8AmountLettuce= u8Amount*INGREDIENT1;
+		u8AmountMeat= u8Amount*INGREDIENT2;
+		u8AmountCheese= u8Amount*INGREDIENT2;
+		u8AmountBread= u8Amount*INGREDIENT1;
+		u8AmountTomato= u8Amount*INGREDIENT1;
+		if(u8AmountLettuce > u8Ingredients[LETTUCE] || u8AmountTomato > u8Ingredients[TOMATO] || u8AmountMeat > u8Ingredients[MEAT] || u8AmountBread > u8Ingredients[BREAD] || u8AmountCheese > u8Ingredients[CHEESE])
+		{
+			printf("Sorry we don't have enough ingredients\n");
+		}
+		else
+		{
+			printf("Please wait until your burgers are ready\n");
+			u8Ingredients[LETTUCE] -= u8AmountLettuce;
+			u8Ingredients[TOMATO] -= u8AmountTomato;
+			u8Ingredients[MEAT] -= u8AmountMeat;
+			u8Ingredients[BREAD] -= u8AmountBread;
+			u8Ingredients[CHEESE] -= u8AmountCheese;
+		}
+	}
+	else if(u8Type==OPTION2)
+	{
+		u8AmountLettuce= u8Amount*INGREDIENT1;
+		u8AmountMeat= u8Amount*INGREDIENT1;
+		u8AmountCheese= u8Amount*INGREDIENT3;
+		u8AmountBread= u8Amount*INGREDIENT1;
+		u8AmountTomato= u8Amount*INGREDIENT1;
+		if(u8AmountLettuce > u8Ingredients[LETTUCE] || u8AmountTomato > u8Ingredients[TOMATO] || u8AmountMeat > u8Ingredients[MEAT] || u8AmountBread > u8Ingredients[BREAD] || u8AmountCheese > u8Ingredients[CHEESE])
+		{
+			printf("Sorry we don't have enough ingredients\n");
+		}
+		else
+		{
+			printf("Please wait until your burgers are ready\n");
+			u8Ingredients[LETTUCE] -= u8AmountLettuce;
+			u8Ingredients[TOMATO] -= u8AmountTomato;
+			u8Ingredients[MEAT] -= u8AmountMeat;
+			u8Ingredients[BREAD] -= u8AmountBread;
+			u8Ingredients[CHEESE] -= u8AmountCheese;
+		}
+	}
+	else if(u8Type==OPTION3)
+	{
+		u8AmountLettuce= u8Amount*INGREDIENT2;
+		u8AmountMeat= u8Amount*INGREDIENT3;
+		u8AmountCheese= u8Amount*INGREDIENT2;
+		u8AmountBread= u8Amount*INGREDIENT1;
+		u8AmountTomato= u8Amount*INGREDIENT2;
+		if(u8AmountLettuce > u8Ingredients[LETTUCE] || u8AmountTomato > u8Ingredients[TOMATO] || u8AmountMeat > u8Ingredients[MEAT] || u8AmountBread > u8Ingredients[BREAD] || u8AmountCheese > u8Ingredients[CHEESE])
+		{
+			printf("Sorry we don't have enough ingredients\n");
+		}
+		else
+		{
+			printf("Please wait until your burgers are ready\n");
+			u8Ingredients[LETTUCE] -= u8AmountLettuce;
+			u8Ingredients[TOMATO] -= u8AmountTomato;
+			u8Ingredients[MEAT] -= u8AmountMeat;
+			u8Ingredients[BREAD] -= u8AmountBread;
+			u8Ingredients[CHEESE] -= u8AmountCheese;
+		}
+	}
+	else 
+	{
+		u8AmountLettuce= u8Amount*INGREDIENT3;
+		u8AmountMeat= u8Amount*INGREDIENT3;
+		u8AmountCheese= u8Amount*INGREDIENT3;
+		u8AmountBread= u8Amount*INGREDIENT1;
+		u8AmountTomato= u8Amount*INGREDIENT3;
+		if(u8AmountLettuce > u8Ingredients[LETTUCE] || u8AmountTomato > u8Ingredients[TOMATO] || u8AmountMeat > u8Ingredients[MEAT] || u8AmountBread > u8Ingredients[BREAD] || u8AmountCheese > u8Ingredients[CHEESE])
+		{
+			printf("Sorry we don't have enough ingredients\n");
+		}
+		else
+		{
+			printf("Please wait until your burgers are ready\n");
+			u8Ingredients[LETTUCE] -= u8AmountLettuce;
+			u8Ingredients[TOMATO] -= u8AmountTomato;
+			u8Ingredients[MEAT] -= u8AmountMeat;
+			u8Ingredients[BREAD] -= u8AmountBread;
+			u8Ingredients[CHEESE] -= u8AmountCheese;
+		}
+	}
+}
+
+void AmountDrinks()
+{	
+	uint8 u8AmountDrinks;
+	printf("How many Drinks do you want?\n");
+	scanf("%d", &u8AmountDrinks);
+	while(u8AmountDrinks<0)
+	{
+		fflush(stdin);
+		printf("Type in a correct number\n");
+		scanf("%d", &u8AmountDrinks);
+	}
+}
+
+void AmountDesserts()
+{
+	uint8 u8AmountDesserts;
+	printf("How many Drinks do you want?\n");
+	scanf("%d", &u8AmountDesserts);
+	while(u8AmountDesserts<0)
+	{
+		fflush(stdin);
+		printf("Type in a correct number\n");
+		scanf("%d", &u8AmountDesserts);
+	}
+}
+
+void AmountFries()
+{
+	uint8 u8AmountFries;
+	printf("How many Friess do you want?\n");
+	scanf("%d", &u8AmountFries);
+	while(u8AmountFries<0)
+	{
+		fflush(stdin);
+		printf("Type in a correct number\n");
+		scanf("%d", &u8AmountFries);
+	}
+}
--- Proyecto Final/McDonalds.h
+++ Proyecto Final/McDonalds.h
@@ -2,12 +2,40 @@ typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 
-#define FACTOR 1
+#define FACTOR 	   1
 #define RESTAURANT 1
 #define DRIVE_THRU 2
-
+#define MAX_LETTUCE 50
+#define MAX_TOMATO  40
+#define MAX_MEAT    100
+#define MAX_BREAD   70
+#define MAX_CHEESE  60
+#define LIMIT1 		1
+#define LIMIT2 		4
+#define OPTION1     1
+#define OPTION2     2
+#define OPTION3     3
+#define OPTION4     4
+#define INGREDIENT1 1
+#define INGREDIENT2 2
+#define INGREDIENT3 3
+#define LETTUCE     0
+#define TOMATO      1
+#define MEAT        2
+#define BREAD       3
+#define CHEESE      4
 
 void Restaurant();
 void Drive_Thru();
+void Menu();
+void Burgers();
+void Drinks();
+void Desserts();
+void Fries();
+uint8 Extra();
+void AmountBurgers(uint8 u8Type);
 void delay(uint8 number_of_seconds);
 uint8 printRandoms(uint8 lower, uint8 upper,  uint8 count);
+void AmountDrinks();
+void AmountDesserts();
+void AmountFries();
